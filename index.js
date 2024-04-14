34e91b21406b79bd7f792eb60f63d7925c4f6723
Farrukh Fida <farrukh.fida@gmail.com>
2024-04-14 22:50:32 +0500

    2nd commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5001))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Deployed via Jenkins pipeline on AWS ec2 instance, for uat.')
+  response.send('Deployed via Jenkins pipeline on AWS ec2 instance, for uat. 2nd try')
 })
 
 app.listen(app.get('port'), function() {
